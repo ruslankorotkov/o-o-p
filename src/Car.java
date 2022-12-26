69afa3ea9c76335cd96f65e2041630d2edba2b3f
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-26 23:24:33 +0300

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,81 @@
+public class Car {
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int productionYear;
+    String productionCountry;
+
+    Car(String brand, String model, String productionCountry, double engineVolume, int productionYear, String color) {
+
+        if (brand == null || brand == "") {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null || model == "") {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (productionCountry == null || productionCountry == "") {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+
+        if (productionYear <= 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        if (color == null || color == "") {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+
+        }
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    @Override
+    public String toString() {
+        return "Automobile{" +
+                "бренд'" + brand + '\'' +
+                ", модель'" + model + '\'' +
+                ", объем двигателя в литрах " + engineVolume +
+                ", цвет кузова '" + color + '\'' +
+                ", год производства  " + productionYear +
+                ", сборка'" + productionCountry + '\'' +
+                '}';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        Car lada = new  Car("Lada","Granta","в России",1.7,2015,"желтый");
+        Car audi = new  Car("Audi","A8 50 L TDI quattro","в Германии", 3.0,2020,"черный" );
+        Car bmv = new  Car("BMW","Z8","в Германии",3.0,2021,"черный");
+        Car kia = new  Car("Kia","Sportage 4-го поколения","в Южной Корее",2.4,2018,"красный");
+        Car Hyundai = new  Car("Hyundai","Avante","в Южной Корее",1.6,2016,"оранжевый");
+        System.out.println(lada);
+        System.out.println(audi );
+        System.out.println(bmv);
+        System.out.println(kia);
+        System.out.println(Hyundai);
+    }
+}
